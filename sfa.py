a659ae3efb1389d876161c2cb2301eba6b2da810
Andrew <amcmahon@bnl.gov>
2022-03-09 18:06:45 -0500

    Adds reports, full sync, SFA forecast replacment

--- sfa.py
+++ sfa.py
@@ -40,11 +40,22 @@ def readConfig():
             sfaConnection["cachedToken"] = cp["sfaInfo"]["cachedToken"]
             sfaConnection["maxTries"] = cp["sfaInfo"]["maxTries"]
 
+            reports = {}
+            reports["provider"] = le(cp["reports"]["provider"])
+            reports["variables"] = le(cp["reports"]["variables"])
+            reports["metrics"] = le(cp["reports"]["metrics"])
+            reports["lead_times"] = le(cp["reports"]["lead_times"])
+
+            forecast = {}
+            forecast["overwrite_existing"] = le(cp["forecast"]["overwrite_existing"])
+            forecast["sensors"] = le(cp["forecast"]["sensors"])
+            forecast["config_name"] = cp["forecast"]["config_id"]
+
         except KeyError as e:
             logging.error("Missing config information: " + str(e))
             return
         
-        return {"db": dbConnection, "sfa": sfaConnection}
+        return {"db": dbConnection, "sfa": sfaConnection, "reports": reports, "forecast":forecast}
 
     except Exception as e:
         logging.error("Error loading config: " + str(e))
@@ -69,37 +80,7 @@ def chunk(df, n):
     #Credit for the quick one-liner: https://stackoverflow.com/questions/44729727/pandas-slice-large-dataframe-in-chunks  
     return {i+min(n, len(df[i:i+n])):df[i:i+n] for i in range(0,df.shape[0],n)}
 
-
 def syncObs():
-    startdt = datetime.now().strftime("%Y-%m-%d_%H%M%S")
-    logFile = "Log_sfaSync_" + startdt + ".log"
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s\t%(message)s', handlers=[logging.FileHandler(logFile), logging.StreamHandler()])
-    if config is None:
-        config = readConfig()
-
-    #Get nowcast metadata
-    db_engine = DBengine(config["db"])
-    sites = db_engine.getSites()
-    sensors = db_engine.getSensors()
-    forecast_ids = db_engine.getForecastIDs()
-
-    #Get SFA metadata
-    sfa_session = SFA(config["sfa"])
-    sfa_sites = sfa_session.getSites()
-    sfa_obs = sfa_session.listObservations()
-    sfa_forecasts = sfa_session.listForecasts()
-
-    site_d = {n.name: n for n in sfa_sites}
-    obs_d = {n.name: n for n in sfa_obs}
-    
-
-    #Sync sites
-    for s in sites:
-        if s["name"] not in site_d.keys():                            #Check which records are new    ****FUTURE: Should change to use UUID instead/in addition to****
-            site_result = sfa_session.addSite(s)                      #Add missing sites
-            db_engine.updateSite(site_result)
-            site_d.update({s["name"]:site_result})                    #Add to local site dict
-
     for sensor in sensors:
         #Sync observation metadata
         sensor["obs_name"] = sensor["site_name"] + " " + sensor["measurement"]  #Site name + measurement should be used for all observation names (ex: "LISF PB1 GHI")
@@ -122,45 +103,14 @@ def syncObs():
 
     logging.info("Done updating data.")
 
-
 def syncForecasts():
-    startdt = datetime.now().strftime("%Y-%m-%d_%H%M%S")
-    logFile = "Log_sfaSync_" + startdt + ".log"
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s\t%(message)s', handlers=[logging.FileHandler(logFile), logging.StreamHandler()])
-    #if config is None:
-    config = readConfig()
-
-    #Get nowcast metadata
-    db_engine = DBengine(config["db"])
-    sites = db_engine.getSites()
-    #sensors = db_engine.getSensors()
-    forecasts = db_engine.getForecasts()
-
-    #Get SFA metadata
-    sfa_session = SFA(config["sfa"])
-    sfa_sites = sfa_session.getSites()
-    #sfa_obs = sfa_session.listObservations()
-    sfa_forecasts = sfa_session.listForecasts()
-
-    site_d = {n.name: n for n in sfa_sites}
-    #obs_d = {n.name: n for n in sfa_obs}
-    forecasts_d = {n.name: n for n in sfa_forecasts}
-    
-
-    #Sync sites
-    for s in sites:
-        if s["name"] not in site_d.keys():                            #Check which records are new    ****FUTURE: Should change to use UUID instead/in addition to****
-            site_result = sfa_session.addSite(s)                      #Add missing sites
-            db_engine.updateSite(site_result)
-            site_d.update({s["name"]:site_result})                    #Add to local site dict
-
     for forecast in forecasts:
         #Sync forecast metadata
         try:
             site_name = next(item["name"] for item in sites if item["site_id"] == forecast["site_id"])
         except KeyError:
             logging.warning("Site id " + str(forecast["site_id"]) + " not found, skipping.")
-            return
+            continue
 
         forecast["forecast_name"] = site_name + " " + str(forecast["lead_time"]) + "min " + forecast["variable"]  #Site name + lead time + measurement should be used for all forecast names (ex: "LISF PB1 10min GHI")
         forecast["site"] = site_d[site_name]                              #Associate SFA site record
@@ -168,9 +118,21 @@ def syncForecasts():
             result = sfa_session.addForecast(forecast)                    #Add missing forecasts
             #db_engine.updateForecasts(result)                            ****FUTURE: Update local metadata
             forecasts_d.update({forecast["forecast_name"]:result})        #Add to local dict
+        elif config["forecast"]["overwrite_existing"]:
+            logging.info("Existing forecast found for " + forecast["forecast_name"])
+            old_uuid = forecasts_d[forecast["forecast_name"]].forecast_id
+            result = sfa_session.deleteForecast(old_uuid)
+            if result == 204:
+                logging.info("\tSuccessfully deleted " + old_uuid)
+            else:
+                logging.warning("\tError removing existing forecast %s, skipping" % old_uuid)
+                continue
+            result = sfa_session.addForecast(forecast)                    #Add missing forecasts
+            #db_engine.updateForecasts(result)                            ****FUTURE: Update local metadata
+            forecasts_d.update({forecast["forecast_name"]:result})        #Add to local dict
 
         #Sync forecast data
-        forecast_uuid = forecasts_d[forecast["forecast_name"]].forecast_id                               #Get forecast UUID now that all should be in the local dict
+        forecast_uuid = forecasts_d[forecast["forecast_name"]].forecast_id                            #Get forecast UUID now that all should be in the local dict
         forecast["start_dt"], forecast["end_dt"] = sfa_session.getForecastTimeRange(forecast_uuid)    #Get existing data range (to be excluded from upload)
         data = db_engine.getForecastData(forecast)                         #Warning: Could be very large
         filled_data = fillMissing(data, method='nearest', useRounding=True)
@@ -182,6 +144,65 @@ def syncForecasts():
 
     logging.info("Done updating data.")
 
+def syncSites():
+    for s in sites:
+        if s["name"] not in site_d.keys():                            #Check which records are new    ****FUTURE: Should change to use UUID instead/in addition to****
+            site_result = sfa_session.addSite(s)                      #Add missing sites
+            db_engine.updateSite(site_result)
+            site_d.update({s["name"]:site_result})                    #Add to local site dict
+
+def generateReports():
+    logging.info("Generating reports...\n\tUpdating observation and forecast lists")
+    sfa_obs = sfa_session.listObservations()
+    sfa_forecasts = sfa_session.listForecasts()
+
+    r_sensor_sites = {s["site_id"]:s for s in sensors if s["sensor_id"] in config["forecast"]["sensors"]}
+    r_sites = {s["name"]:s for s in sites if s["site_id"] in r_sensor_sites.keys()}
+    sfa_forecasts = [f for f in sfa_forecasts if f.provider==config["reports"]["provider"]
+                                                 and int(f.lead_time_to_start.total_seconds()/60) in config["reports"]["lead_times"]
+                                                 and f.site.name in r_sites.keys()]
+    obs_d = {n.name: n for n in sfa_obs}
+    forecasts_d = {n.name: n for n in sfa_forecasts}
+    logging.info("Found %i observations and %i forecasts" % (len(sfa_obs), len(sfa_forecasts)))
+
+    for lead_time in config["reports"]["lead_times"]:
+        forecasts_grouped = {k:f for k,f in forecasts_d.items() if int(f.lead_time_to_start.total_seconds()/60)==lead_time}
+        logging.info("  For %i min lead time, %i forecasts" % (lead_time, len(forecasts_grouped)))
+
+        forecast_obs = []
+        r_name = config["forecast"]["config_name"] + " " + str(lead_time) + "min"
+        r_start = None
+        r_end = None
+
+        for f_name, sfa_for in forecasts_grouped.items():
+            sfa_site = sfa_for.site
+            obs_name = sfa_site.name + " " + sfa_for.variable.upper()
+            f_name = sfa_for.name
+
+            try:
+                obs = obs_d[obs_name]
+            except KeyError:
+                logging.info("\t%s: No matching observation '%s' found, skipping" % (f_name, obs_name))
+                continue
+
+            f_start, f_end = sfa_session.getForecastTimeRange(sfa_for.forecast_id)
+            try:
+                r_start = min(r_start, f_start)
+                r_end = max(r_end, f_end)
+            except TypeError:
+                r_start = f_start
+                r_end = f_end
+
+            forecast_obs += [sfa_dm.ForecastObservation(sfa_for, obs)]
+            logging.info("\t%s: Added to report" % f_name)
+
+        logging.info("Generating report for %d forecasts from %s to %s" % (len(forecast_obs), r_start.strftime("%Y-%m-%d %H:%M:%S"), r_end.strftime("%Y-%m-%d %H:%M:%S")))
+        result = sfa_session.generateReport(r_name, r_start, r_end, forecast_obs)
+        try:
+            logging.info("Report ID: %s\tStatus: %s" % (result.report_id, result.status))
+        except Exception as e:
+            logging.warning("\tError generating report, did not receive info from Arbiter")
+
 
 class SFA():
 
@@ -189,7 +210,9 @@ class SFA():
         self.max_tries = config["maxTries"]
         self.upload_error_count = {}            #Tracks upload errors per observation
         try:
-            sfa_tok = config["cachedToken"]
+            #sfa_tok = config["cachedToken"]
+            with open("sfa_token.txt", 'r') as f:
+                sfa_tok = f.readline()
             self.session = sfa_api.APISession(sfa_tok)
             self.session.get_user_info()        #Make sure token is still valid
             logging.info("Using cached token")
@@ -198,7 +221,10 @@ class SFA():
             logging.info("Getting new token")
             sfa_tok = sfa_api.request_cli_access_token(config["user"],config["password"])
             self.session = sfa_api.APISession(sfa_tok)
-            logging.info("SFA Token: " + sfa_tok)
+            with open("sfa_token.txt", 'w') as f:
+                f.write(sfa_tok)
+                logging.info("\tCached for future requests")
+            logging.debug("SFA Token: " + sfa_tok)
         self.cachedToken = sfa_tok
 
     def getSites(self):
@@ -259,10 +285,14 @@ class SFA():
     def addForecast(self, forecast):
         sfa_obs = sfa_dm.Forecast(name=forecast["forecast_name"], variable=forecast["variable"].lower(), issue_time_of_day=forecast["issue_start_datetime"].round('1min').strftime("%H:%M"),
                                   lead_time_to_start=pd.Timedelta(minutes=forecast["lead_time"]), run_length=pd.Timedelta(minutes=1),
-                                  interval_value_type='instantaneous', interval_length=pd.Timedelta('0 days 00:01:00'), interval_label='instant', site=forecast["site"])
+                                  interval_value_type='interval_mean', interval_length=pd.Timedelta('0 days 00:01:00'), interval_label='ending', site=forecast["site"])
         logging.info("Adding forecast: " + str(sfa_obs))
         sfa_obs = self.session.create_forecast(sfa_obs)
         return sfa_obs
+
+    def deleteForecast(self, forecast_id):
+        result = self.session.delete_forecast(forecast_id)
+        return result
         
     def addForecastData(self, df, forecast_uuid):
         #temp = df.copy()    #Seems like there should be a more effecient way to do this, but since df is already a slice...
@@ -280,6 +310,43 @@ class SFA():
                 self.upload_error_count[forecast_uuid] = 1
         return
 
+    def generateReport(self, r_name, r_start, r_end, forecast_obs):
+
+        report_params = sfa_dm.ReportParameters(name=r_name, start=r_start, end=r_end, object_pairs=forecast_obs,
+                 metrics=config["reports"]["metrics"], categories=('total', 'date'), forecast_fill_method='drop')
+
+        report = sfa_dm.Report(report_params)
+        result = self.session.create_report(report)
+
+        return result
+        
+
+
 if __name__ == "__main__":
+
+    startdt = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+    logFile = "Log_sfaSync_" + startdt + ".log"
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s\t%(message)s', handlers=[logging.FileHandler(logFile), logging.StreamHandler()])
+
+    config = readConfig()
+
+    #Get nowcast metadata
+    db_engine = DBengine(config["db"])
+    sites = db_engine.getSites()
+    sensors = db_engine.getSensors()
+    forecasts = db_engine.getForecasts()
+
+    #Get SFA metadata
+    sfa_session = SFA(config["sfa"])
+    sfa_sites = sfa_session.getSites()
+    sfa_obs = sfa_session.listObservations()
+    sfa_forecasts = sfa_session.listForecasts()
+
+    site_d = {n.name: n for n in sfa_sites}
+    obs_d = {n.name: n for n in sfa_obs}
+    forecasts_d = {n.name: n for n in sfa_forecasts}
+
+    syncSites()
     #syncObs()
-    syncForecasts()
+    #syncForecasts()
+    generateReports()
